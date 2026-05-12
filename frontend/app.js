8e6b01420f4498ab1afd4fc189f4b651bd6c5742
wendelrocha14 <wendeldarocha2@gmail.com>
2026-05-12 00:59:42 +0000

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -43,7 +43,7 @@ if (form) {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/appointments/", {
+      const res = await fetch("/appointments/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
